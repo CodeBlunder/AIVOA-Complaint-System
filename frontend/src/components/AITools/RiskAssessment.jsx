a6606e64d1da63835b391e87f84986d604e0140a
CodeBlunder <vvce23cse0131@vvce.ac.in>
2026-07-29 20:38:52 +0530

    Update RiskAssessment.jsx

--- frontend/src/components/AITools/RiskAssessment.jsx
+++ frontend/src/components/AITools/RiskAssessment.jsx
@@ -1,4 +1,4 @@
-// src/components/AITools/RiskAssessment.jsx
+
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { AlertTriangle, CheckCircle, ShieldAlert, FileText } from 'lucide-react';
