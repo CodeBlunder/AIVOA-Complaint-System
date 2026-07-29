380c3fd273ef6a16411fee43588121bc9fb6d901
CodeBlunder <vvce23cse0131@vvce.ac.in>
2026-07-29 20:42:57 +0530

    Update ComplaintsList.jsx

--- frontend/src/pages/ComplaintsList.jsx
+++ frontend/src/pages/ComplaintsList.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ComplaintList from '../components/Complaint/ComplaintList';
 
-/**
- * All Complaints page – thin wrapper around the ComplaintList component.
- */
+
 export default function ComplaintsList() {
   return (
     <div>
